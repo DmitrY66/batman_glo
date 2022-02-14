50594fb4201a51c3ba7aaf7b81354b9b659d7330
Dmitri Gorun <iii448@mail.ru>
2022-02-14 18:42:05 +0500

    fifth commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const swiper = new Swiper('.main__slider', {
   slidesPerView: 2,
 
   spaceBetween: 30,
-  
+
   loop: true,
 
   navigation: {
@@ -11,7 +11,7 @@ const swiper = new Swiper('.main__slider', {
   },
 
   breakpoints: {
-    320: {
+    120: {
       slidesPerView: 1,
     },
     768: {
@@ -25,9 +25,23 @@ const headerMenuBurger = document.querySelector('.header__menu-burger');
 const headerMenuClose = document.querySelector('.header__menu-close');
 const header = document.querySelector('.header');
 
+const modal = document.querySelector('.modal');
+const mainButtonPlay = document.querySelector('.main__button-play');
+
 headerMenuBurger.addEventListener('click', () => {
   header.style.display = 'block';
 })
 headerMenuClose.addEventListener('click', () => {
   header.style.display = 'none';
+})
+
+mainButtonPlay.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.add('active');
+})
+modal.addEventListener('click', (e) => {
+  const isModal = e.target.closest('.modal__inner');
+  if (!isModal) {
+    modal.classList.remove('active');
+  }
 })

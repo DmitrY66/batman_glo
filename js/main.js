7079168c87b7102a7821106896dabee7b990112e
Dmitri Gorun <iii448@mail.ru>
2022-02-14 17:48:11 +0500

    fourth commit

--- js/main.js
+++ js/main.js
@@ -10,4 +10,24 @@ const swiper = new Swiper('.main__slider', {
     nextEl: '.main__slider-arrow',
   },
 
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  }
+
 });
+
+const headerMenuBurger = document.querySelector('.header__menu-burger');
+const headerMenuClose = document.querySelector('.header__menu-close');
+const header = document.querySelector('.header');
+
+headerMenuBurger.addEventListener('click', () => {
+  header.style.display = 'block';
+})
+headerMenuClose.addEventListener('click', () => {
+  header.style.display = 'none';
+})
